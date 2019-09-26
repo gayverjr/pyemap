c604bba2e1121918f6f1d281ef3887214517d46d
James Gayvert <jrg444@gmail.com>
2019-09-26 00:11:24 -0400

    Switched to better PDB for emap object test cases.

--- pyemap/tests/test_emap.py
+++ pyemap/tests/test_emap.py
@@ -6,23 +6,23 @@ import pyemap
 import tempfile
 
 def test_save_functions():
-    my_emap = pyemap.parse(os.path.join(sys.path[0],"pyemap/tests/test_pdbs/1dnp.pdb")) 
+    my_emap = pyemap.parse(os.path.join(sys.path[0],"pyemap/tests/test_pdbs/4dja.pdb")) 
     #cluster
     fout = tempfile.NamedTemporaryFile(suffix=".png")
-    my_emap.residue_to_Image("MHF473(B)")
-    my_emap.residue_to_file("MHF473(B)",dest=fout.name)
+    my_emap.residue_to_Image("SF4603(A)")
+    my_emap.residue_to_file("SF4603(A)",dest=fout.name)
     #aromatic eta moiety
-    my_emap.residue_to_Image("FAD472(B)-1")
-    my_emap.residue_to_file("FAD472(B)-1",dest=fout.name)
+    my_emap.residue_to_Image("FAD601(A)-1")
+    my_emap.residue_to_file("FAD601(A)-1",dest=fout.name)
     #after file is processed
     pyemap.process(my_emap)
     #standard residue
-    my_emap.residue_to_Image("W306(A)")
-    my_emap.residue_to_file("FAD472(B)-1",dest=fout.name)
+    my_emap.residue_to_Image("Y443(A)")
+    my_emap.residue_to_file("Y443(A)",dest=fout.name)
     #init graph
     my_emap.init_graph_to_Image()
     my_emap.init_graph_to_file(dest=fout.name)
-    pyemap.find_paths(my_emap,"FAD472(A)-2")
+    pyemap.find_paths(my_emap,"Y443(A)")
     #paths graph
     my_emap.paths_graph_to_Image()
     my_emap.paths_graph_to_file(dest=fout.name)
