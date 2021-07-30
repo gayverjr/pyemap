3ecc070fe1a0159c1787bacc8b358dce63bd30e6
James Gayvert <jrg444@gmail.com>
2021-07-30 14:45:43 -0400

    fixed spacing

--- pyemap/tests/test_pathways.py
+++ pyemap/tests/test_pathways.py
@@ -14,12 +14,6 @@ class PathwaysTest(unittest.TestCase):
         elif platform == "darwin":
             pyemap.process(cls.my_emap)
 
-'''  commented out until CI for dssp/msms can be resolved
-    def test_dijkstras_paths(self):
-        pyemap.find_paths(self.my_emap,"Y167(B)")
-        assert len(self.my_emap.paths)>0
-'''
-
     def test_yens_paths(self):
         pyemap.find_paths(self.my_emap,"Y167(B)",target="Y369(B)")
         assert len(self.my_emap.paths)==10
@@ -30,6 +24,12 @@ class PathwaysTest(unittest.TestCase):
             assert False
         except Exception as e:
             assert True
+
+'''  commented out until CI for dssp/msms can be resolved
+    def test_dijkstras_paths(self):
+        pyemap.find_paths(self.my_emap,"Y167(B)")
+        assert len(self.my_emap.paths)>0
+'''
 
 
 
